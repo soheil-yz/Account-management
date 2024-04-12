71b0b2321ea0e2dd030892f41bedb2e22f65e89c
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-04-13 02:18:49 +0330

    Create MainScreen File And Start to Complate Application

---
+++ lib/Constans.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const KPurpleColor = Color(0xFF6C63FF);
+const KRedColor = Color(0xFFFF2D55);
+const KGreenColor = Color(0xFF00C89C);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:money_managment/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'مدیرت مالی',
+      home: MainScreen(),
+    );
   }
 }
