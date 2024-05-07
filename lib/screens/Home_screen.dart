613c68f0944ca06a50b37bf4546ab8b3735a8f1d
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2024-05-07 12:31:08 +0330

    Improve searchBar

--- lib/screens/Home_screen.dart
+++ lib/screens/Home_screen.dart
@@ -1,5 +1,7 @@
 //import 'dart:js_interop';
 
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -21,7 +23,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController SearchController = TextEditingController();
-    Box<Money> hiveBox = Hive.box<Money>('moneyBox');
+  Box<Money> hiveBox = Hive.box<Money>('moneyBox');
 
   @override
   void initState() {
@@ -35,122 +37,159 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         //backgroundColor: Colors.indigo[200],
         floatingActionButton: FloatingActionButton(
-            backgroundColor: KPurpleColor,
-            elevation: 0,
-            onPressed: () {
-              NewScreen.descriptionController.text = '';
-              NewScreen.priceController.text = '';
-              NewScreen.groupId = 0;
-              NewScreen.isEditing = false;
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => NewScreen(),
-                ),
-              ).then((value){ 
+          backgroundColor: KPurpleColor,
+          elevation: 0,
+          onPressed: () {
+            NewScreen.descriptionController.text = '';
+            NewScreen.priceController.text = '';
+            NewScreen.groupId = 0;
+            NewScreen.isEditing = false;
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewScreen(),
+              ),
+            ).then(
+              (value) {
                 setState(
                   () {
                     MyApp.getData();
                   },
                 );
-            });
-            },
-            child: const Icon(
-              Icons.add,
-            )),
+              },
+            );
+          },
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
         body: SizedBox(
           width: double.infinity,
           child: Column(
             children: [
-              HeaderWidget(SearchController: SearchController),
+              HeaderWidget(),
               //const Expanded(child: EmptyWight()),
               Expanded(
-                child: hiveBox.values.isEmpty
+                child: HomeScreen.moneys.isEmpty
                     ? EmptyWight()
                     : ListView.builder(
-                        itemCount: hiveBox.values.length,
+                        itemCount: HomeScreen.moneys.length,
                         itemBuilder: (context, index) {
                           return GestureDetector(
-                              //!Edit
-                              onDoubleTap: () {
-                                NewScreen.descriptionController.text =
-                                    HomeScreen.moneys[index].title;
-                                NewScreen.priceController.text =
-                                    HomeScreen.moneys[index].price;
-                                NewScreen.groupId =
-                                    HomeScreen.moneys[index].isReceived ? 1 : 2 ;
-                                NewScreen.isEditing = true;
-                                NewScreen.index = index;
+                            //!Edit
+                            onDoubleTap: () {
+                              NewScreen.descriptionController.text =
+                                  HomeScreen.moneys[index].title;
+                              NewScreen.priceController.text =
+                                  HomeScreen.moneys[index].price;
+                              NewScreen.groupId =
+                                  HomeScreen.moneys[index].isReceived ? 1 : 2;
+                              NewScreen.isEditing = true;
+                              NewScreen.index = index;
 
-
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const NewScreen(),
-                                  ),
-                                ).then((value) {
-                                  
-                                  MyApp.getData();
-                                    setState(() {
-                                    });
-                                });
-                              },
-                              //!Removed
-                              onTap: () {
-                                showDialog(
-                                    context: context,
-                                    builder: (context) => AlertDialog(
-                                          title: const Text(
-                                              "Are you sure you want to delete it?",
-                                              style: TextStyle(fontSize: 15)),
-                                          actions: [
-                                            Padding(
-                                              padding: EdgeInsets.only(top: 20),
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: [
-                                                  TextButton(
-                                                    onPressed: () {
-                                                      setState(() {
-                                                          hiveBox.deleteAt(index);
-                                                          MyApp.getData();
-                                                        // HomeScreen.moneys
-                                                        //     .removeAt(index);
-                                                        Navigator.pop(context);
-                                                      });
-                                                    },
-                                                    child: Text(
-                                                      "YES",
-                                                      style: TextStyle(
-                                                          color:
-                                                              Colors.black87),
-                                                    ),
-                                                  ),
-                                                  TextButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context);
-                                                    },
-                                                    child: Text(
-                                                      "NO",
-                                                      style: TextStyle(
-                                                          color:
-                                                              Colors.black87),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const NewScreen(),
+                                ),
+                              ).then((value) {
+                                MyApp.getData();
+                                setState(() {});
+                              });
+                            },
+                            //!Removed
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: const Text(
+                                      "Are you sure you want to delete it?",
+                                      style: TextStyle(fontSize: 15)),
+                                  actions: [
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 20),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          TextButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                hiveBox.deleteAt(index);
+                                                MyApp.getData();
+                                                // HomeScreen.moneys
+                                                //     .removeAt(index);
+                                                Navigator.pop(context);
+                                              });
+                                            },
+                                            child: Text(
+                                              "YES",
+                                              style: TextStyle(
+                                                  color: Colors.black87),
                                             ),
-                                          ],
-                                        ));
-                              },
-                              child: MyListTileWedget(index: index));
-                        }),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: Text(
+                                              "NO",
+                                              style: TextStyle(
+                                                  color: Colors.black87),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                            child: MyListTileWedget(index: index),
+                          );
+                        },
+                      ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget HeaderWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 20, top: 20, left: 5),
+      child: Row(
+        children: [
+          Expanded(
+            child: SearchBarAnimation(
+              textEditingController: SearchController,
+              isOriginalAnimation: false,
+              trailingWidget: Icon(Icons.search),
+              secondaryButtonWidget: Icon(Icons.search_off),
+              buttonWidget: Icon(Icons.search),
+              buttonElevation: 0,
+              onFieldSubmitted: (String text) {
+                List<Money> result = hiveBox.values
+                    .where((element) =>
+                        element.title.contains(text) ||
+                        element.date.contains(text))
+                    .toList();
+                HomeScreen.moneys.clear();
+                setState(() {
+                  for (var item in result) {
+                    HomeScreen.moneys.add(item);
+                  }
+                });
+              },
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Text('Transactions'),
+        ],
       ),
     );
   }
@@ -217,39 +256,59 @@ class MyListTileWedget extends StatelessWidget {
   }
 }
 
-class HeaderWidget extends StatelessWidget {
-  const HeaderWidget({
-    super.key,
-    required this.SearchController,
-  });
+// class HeaderWidget extends StatefulWidget {
+//   HeaderWidget({
+//     super.key,
+//     required this.SearchController,
+//   });
 
-  final TextEditingController SearchController;
+//   final TextEditingController SearchController;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 20, top: 20, left: 5),
-      child: Row(
-        children: [
-          Expanded(
-            child: SearchBarAnimation(
-              textEditingController: SearchController,
-              isOriginalAnimation: false,
-              trailingWidget: Icon(Icons.search),
-              secondaryButtonWidget: Icon(Icons.search_off),
-              buttonWidget: Icon(Icons.search),
-              buttonElevation: 0,
-            ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          const Text('Transactions'),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   State<HeaderWidget> createState() => _HeaderWidgetState();
+// }
+
+// class _HeaderWidgetState extends State<HeaderWidget> {
+//   Box<Money> hiveBox = Hive.box<Money>('moneyBox');
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//   padding: const EdgeInsets.only(right: 20, top: 20, left: 5),
+//   child: Row(
+//     children: [
+//       Expanded(
+//         child: SearchBarAnimation(
+//           textEditingController: widget.SearchController,
+//           isOriginalAnimation: false,
+//           trailingWidget: Icon(Icons.search),
+//           secondaryButtonWidget: Icon(Icons.search_off),
+//           buttonWidget: Icon(Icons.search),
+//           buttonElevation: 0,
+//           onFieldSubmitted: (String text) {
+//             List<Money> result = hiveBox.values
+//                 .where((element) =>
+//                     element.title.contains(text) ||
+//                     element.date.contains(text))
+//                 .toList();
+//                 HomeScreen.moneys.clear();
+//                 setState(() {
+//                   for (var item in result){
+//                     HomeScreen.moneys.add(item);
+//                   }
+//                 });
+//           },
+//         ),
+//       ),
+//       const SizedBox(
+//         width: 10,
+//       ),
+//       const Text('Transactions'),
+//     ],
+//   ),
+// );
+//   }
+// }
 
 class EmptyWight extends StatelessWidget {
   const EmptyWight({super.key});
